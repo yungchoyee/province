88fb116e0b70b15648fcb0f78ce62d8c9b5d3420
吕鑫 <lvxinjoey@foxmail.com>
2020-07-16 20:09:06 +0800

    2

--- updateLivePlace.py
+++ updateLivePlace.py
@@ -11,10 +11,35 @@ class handleData:
         print('正在执行，请稍等....')
         print('当前时间：' + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         self.startTime = time.time()
-        self.conn = mysql.connector.connect(user=dbConfig["user"], password=dbConfig["password"], database=dbConfig["shop"])
+        self.conn = mysql.connector.connect(**dbConfig)
         self.cursor = self.conn.cursor()
     def __del__(self):
-        self.conn.close()
+        # self.conn.close()
+        pass
 
-    def updateRoomLivePlaceId(self,roomId):
-        self.cursor.execute("")
+    def updateRoomLivePlaceId(self):
+        self.cursor.execute("SELECT live_place FROM x_live_room WHERE province_id = 0 LIMIT 1")
+        livePlace = self.cursor.fetchone()
+
+        if len(livePlace) > 0:
+            livePlace = livePlace[0].split(" ")
+            provinceName = livePlace[0]
+            cityName = livePlace[1]
+            areaName = livePlace[2]
+            self.cursor.execute("SELECT regionid,parentid FROM xcms_region WHERE region_name = %s",[areaName])
+            area = self.cursor.fetchone()
+            if len(area) > 0:
+                print(area[0])
+
+
+
+dbConfig = {
+    "host":"81.68.71.30",
+    "user":"root",
+    "password":"tdsql-jrb88cto",
+    "port":3306,
+    "database":"shop",
+    "charset":"utf8",
+}
+handleData(dbConfig).updateRoomLivePlaceId()
+
